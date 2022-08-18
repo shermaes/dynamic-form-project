020b7595e2c4efad3c43de468fdf164e969c2c6e
Sherilyn Maestre <you@example.com>
2022-08-18 13:12:56 -0500

    created the hobbies step component

--- src/components/DynamicForm/SecondStep/SecondStep.js
+++ src/components/DynamicForm/SecondStep/SecondStep.js
@@ -1,7 +1,52 @@
+import { Col, Container, Form, Row } from "react-bootstrap"
 import "./SecondStep.css"
+import Select from "react-select";
+import { useState } from "react";
 
 export default function SecondStep() {
+
+  //array de objetos para esta seccion del form
+  const options = [
+    { id: 1, value: "football", label: "Football ⚽" },
+    { id: 2, value: "computer", label: "Computer 💻" },
+    { id: 3, value: "play", label: "Play Videogames 🎮" },
+    { id: 4, value: "music", label: "Play Music 🎼" },
+    { id: 5, value: "photo", label: "Photography 📷" },
+    { id: 6, value: "swim", label: "Swim 🥽" },
+    { id: 7, value: "run", label: "Run 👟" },
+  ];
+
+  const [selectOption, setSelectOption] =useState(null)
+  //neceistamos esto para recorrer el array de objetos
+  const hobbiesSer= JSON.stringify(selectOption)
+  localStorage.setItem('hobbies', hobbiesSer)
+
+  const onChangeAdditionalHobbie = (e) =>{
+    const sport = e.target.value;
+    //guardando en el localStorage, guardo todoa la informacion de los objetos seleccionados desde la pag
+    localStorage.setItem('adittionalHobbie', sport)
+  }
   return (
-    <div>SecondStep</div>
+    <div>
+      <h2>Select your favorite Hobbies</h2>
+      <h4>Tell me about you</h4>
+      <Container className="p-5 text-center">
+        <Row className="justify-content-md-center">
+          <Col lg="7">
+          <Select
+              defaultValue={selectOption}
+              onChange={setSelectOption}
+              options={options}
+              isSearchable
+              isMulti
+            />
+          </Col>
+          <Col lg="7" className="mt-4">
+            <h5>Not there? Write it down here</h5>
+            <Form.Control type="text" placeholder="Karate" onChange={(e) => onChangeAdditionalHobbie(e)}/>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
